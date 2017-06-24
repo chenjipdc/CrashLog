b01720ebed54c858ce3069c579fd0ab35e6a4332
pdc <cj@pdc.local>
2017-06-24 14:55:11 +0800

    add connected

---
+++ CrashLog/RSCrashConfiguration.h
@@ -0,0 +1,13 @@
+//
+//  RSCrashConfiguration.h
+//  CrashLogDemo
+//
+//  Created by pdc on 2017/6/24.
+//  Copyright © 2017年 RealsCloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSCrashConfiguration : NSObject
+
+@end
---
+++ CrashLog/RSCrashConfiguration.m
@@ -0,0 +1,13 @@
+//
+//  RSCrashConfiguration.m
+//  CrashLogDemo
+//
+//  Created by pdc on 2017/6/24.
+//  Copyright © 2017年 RealsCloud. All rights reserved.
+//
+
+#import "RSCrashConfiguration.h"
+
+@implementation RSCrashConfiguration
+
+@end
